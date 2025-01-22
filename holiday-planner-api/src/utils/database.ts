e57a7966aa4eaec12c0290ef11f5564cb793534d
Anisha Norman <ajnorman@hotmail.co.uk>
2025-01-22 18:07:30 +0000

    Update database config for AWS deployment

--- holiday-planner-api/src/utils/database.ts
+++ holiday-planner-api/src/utils/database.ts
@@ -1,11 +1,17 @@
-import { Sequelize } from 'sequelize';
-import * as dotenv from 'dotenv';
+import * as dotenv from "dotenv";
+import { Sequelize } from "sequelize";
 
 dotenv.config();
 
 const sequelize = new Sequelize(process.env.DATABASE_URL!, {
-  dialect: 'postgres',
-  logging: false,
+	dialect: "postgres",
+	logging: false,
+	dialectOptions: {
+		ssl: {
+			require: true,
+			rejectUnauthorized: false,
+		},
+	},
 });
 
 export default sequelize;
